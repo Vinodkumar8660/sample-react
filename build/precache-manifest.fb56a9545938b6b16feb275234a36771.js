b8a24c13888f59d2202136abd69d8c3352efaaa4
Vinodkumar8660 <vinodkumarthotakura2112@gmail.com>
2022-06-09 22:33:48 +0530

    Updates

---
+++ build/precache-manifest.fb56a9545938b6b16feb275234a36771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32d38aafab9d1b5da5e5",
+    "url": "/static/css/main.9fe32a6d.chunk.css"
+  },
+  {
+    "revision": "32d38aafab9d1b5da5e5",
+    "url": "/static/js/main.32d38aaf.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "6d0b5880cf9c8a0d76a3a93774316f92",
+    "url": "/index.html"
+  }
+];
